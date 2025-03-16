f13df7f80a15fddae8861f8aebe086ed0681b75d
henriquedeantoni <henriqdeantoni@gmail.com>
2025-03-15 23:57:42 -0300

    feat: nova classe genero em modelos

---
+++ ScreenSoundShared.Modelos/Modelos/Genero.cs
@@ -0,0 +1,21 @@
+﻿using ScreenSound.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScreenSoundShared.Modelos.Modelos
+{
+    internal class Genero
+    {
+        public int Id { get; set; }
+        public string? Nome { get; set; } = string.Empty;
+        public string? Descricao { get; set; } = string.Empty;
+
+        public override string ToString()
+        {
+            return $"Nome: {Nome} - Descrição: {Descricao}";
+        }
+    }
+}
